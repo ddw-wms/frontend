3f8364ff5a0cdeddd6ca929ea51697b36cfb4fbd
ddw-wms <ddw.blr@gmail.com>
2026-01-14 14:30:16 +0530

    dnnn

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+    return (
+        <>
+            <link rel="icon" href="/favicon.ico" />
+            <link rel="apple-touch-icon" href="/file.svg" />
+            <link rel="manifest" href="/site.webmanifest" />
+        </>
+    );
+}
