08b7d3a03a6a6c32a64f7176b4ffbcd51bb47e33
ddw-wms <ddw.blr@gmail.com>
2026-02-05 16:32:45 +0530

    feat: add key option to AppRouterCacheProvider for improved CSS layer handling

--- components/ThemeRegistry.tsx
+++ components/ThemeRegistry.tsx
@@ -713,7 +713,7 @@ export default function ThemeRegistry({ children }: { children: React.ReactNode 
     const theme = React.useMemo(() => createAppTheme(mode), [mode]);
 
     return (
-        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+        <AppRouterCacheProvider options={{ enableCssLayer: true, key: 'mui' }}>
             <ThemeModeContext.Provider value={{ mode, toggleMode, setMode }}>
                 <ThemeProvider theme={theme}>
                     <CssBaseline enableColorScheme />
